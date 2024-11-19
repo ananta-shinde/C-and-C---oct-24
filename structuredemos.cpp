d9974e5d8f9918f60691e811d634f34b973548cb
aShinde <ananta.shinde@honeywellcom>
2024-11-19 16:07:04 +0530

    19/11/24

--- structuredemos.cpp
+++ structuredemos.cpp
@@ -1,17 +1,26 @@
 #include<stdio.h>
-// define a structure
 struct Student{
     int rollNo;
-    char name[40];
-    char address[100];
+    // char* name;
+    float marks;
+    
 };
-
 int main(int argc, char const *argv[])
 {
-    int numbers[10];
-    // allocation a stucture
-    Student s1;
-    // allocation of array of structure
-    Student list[10];
+    Student student_list[5];
+
+    for(int i=0;i<5;i++){
+         printf("Enter roll marks:");
+       scanf( "%f",&student_list[i].marks);
+        printf("Enter roll No:");
+       scanf( "%d",&student_list[i].rollNo);
+    }
+
+    for(int i=0;i<5;i++){
+        printf("RollNo : %d, Marks: %f\n",student_list[i].rollNo,student_list[i].marks);
+    }
+
     return 0;
 }
+
+
