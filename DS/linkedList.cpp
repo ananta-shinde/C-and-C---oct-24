069db9885823bee794479d206e0dbda753f52f4c
aShinde <ananta.shinde@honeywellcom>
2025-01-14 14:22:21 +0530

    14/05/2025

--- DS/linkedList.cpp
+++ DS/linkedList.cpp
@@ -5,29 +5,64 @@ struct Node
     Node* next= NULL;
 };
 
+Node* insertAtStart(Node* head,int value){
+     Node* newNode = new Node();
+     newNode->data = value;
+     if(head == NULL){
+        head = newNode;
+     }else{
+        newNode->next = head;
+        head = newNode;
+     }
+     return head;
+
+}
+
+Node* insertAtEnd(Node* head,int value){
+    Node* newNode = new Node();
+     newNode->data = value;
+     if(head == NULL){
+        head = newNode;
+     }else{
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+     }
+
+     return head;
+}
+
+int lengthOfList(Node* head){
+    int count = 0;
+    Node* temp = head;
+    while(temp!=NULL){
+        count++;
+        temp =temp->next;
+    }
+    return count;
+}
+
+void printList(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        printf("%d\t",temp->data);
+        temp =temp->next;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     // create empty list
     Node* num_list = NULL;
-
-    // add new node to empty list
-    Node newNode;
-    newNode.data = 20;
-    num_list = &newNode;
-
-
-    // add new node to empty list
-    Node newNode;
-    newNode.data = 40;
-    Node* temp = num_list;
-    num_list = &newNode;
-  
-    // Adding new node to the end of non empty list
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
-    temp->next = &newNode;
-   
+    printf("length:%d\n",lengthOfList(num_list));
+    num_list=insertAtStart(num_list,20);
+    num_list=insertAtStart(num_list,30);
+    num_list=insertAtEnd(num_list,50);
+    printf("length:%d\n",lengthOfList(num_list));
+    printList(num_list);
+    
 
 
 
